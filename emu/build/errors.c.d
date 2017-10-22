9161c12fa1f107fa9534a3b3fa69edcc22a6a99c
alain <alain.delaet-tixeuil@ens-lyon.fr>
2017-10-22 19:37:49 +0200

    réorganisation du dossier

---
+++ emu/build/errors.c.d
@@ -0,0 +1,3 @@
+build/errors.c.o: src/errors.c include/errors.h
+
+include/errors.h:
---
+++ emu/build/main.c.d
@@ -0,0 +1,5 @@
+build/main.c.o: src/main.c include/defs.h include/errors.h
+
+include/defs.h:
+
+include/errors.h:
