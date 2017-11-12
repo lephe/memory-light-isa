cdb93dabca5c27cee2a8e9f383a6d44159b158b3
Black Rook <sebastien.mld@numericable.fr>
2017-11-12 15:59:40 +0100

    Working signed 64 * 64 -> 128 mutliplication.

--- prog/div.s
+++ prog/div.s
@@ -1,5 +1,6 @@
-; Note for teacher: as we have 64-bit registers, we perform 64/64 division.
-; This program has no limitation on the size of the divider.
+;-----------------------------------------------------------------------------;
+;  Signed 64 / 64 -> 64 division                                              ;
+;-----------------------------------------------------------------------------;
 
 ; Initialization (program input)
 	leti	r0 -0x538ba20c467c034b
@@ -12,20 +13,22 @@
 
 ; Manage signs (r4 < 0 iff we need to swap signs after dividing)
 	xor3	r4 r0 r1
-	; We need to have 0 in a register to calcule rx = 0 - rx
+	; We need to have 0 in a register to calculate rx = 0 - rx
 	leti	r5 0
 
 	cmpi	r0 0
-	jumpif	sgt r0_nonnegative
+	jumpif	sgt r0_positive
 	sub3	r0 r5 r0
-r0_nonnegative:
+r0_positive:
 	cmpi	r1 0
-	jumpif	sgt r1_nonnegative
+	jumpif	sgt r1_positive
 	sub3	r1 r5 r1
-r1_nonnegative:
+r1_positive:
 
 ; First, we need to shift r1 left so that its most significant bit reaches
-; position 63. We do this in a naive way, which is costly.
+; position 63. This naive method is costly, however we cannot just compute
+; lg(r1) and shift left by 64 - lg(r1) because we can only perform constant
+; shifts. A logarithmic method is presented below.
 shft:
 	cmpi	r1 0
 	jumpif	slt nonzero
@@ -33,9 +36,21 @@ shft:
 	shift	left r1 1
 	jump	shft
 
-; Then we substract the divisor from the dividend when possible, and we shift
+; Alternative method in C syntax (logarithmic complexity):
+;	x = r1;
+;	if(r1 & 0xffffffff == r1) x <<= 32;
+;	else r1 >>= 32;
+;	if(r1 & 0xffff == r1) x <<= 16;
+;	else r1 >>= 16;
+;	if(r1 & 0xff == r1) x <<= 8;
+;	else r1 >>= 8;
+;	...
+;	if(r1 & 0x1 == r1) x <<= 1;
+;	r1 = x;
+
+; Then we remove the divisor from the dividend when possible, and we shift
 ; right before trying again. This loop runs exactly 63 - lg(r1) times.
-; Compiler announces the size of the loop is 80 bits.
+; The size of this loop is 80 bytes.
 nonzero:
 	; If r1 >= r0, increase result and decrease r1
 	cmp	r0 r1
@@ -49,10 +64,8 @@ next:
 	jumpif	nz nonzero
 
 ; Now swap signs if it was required
-	cmpi	r4 0
-	; Jump if r4 >= 0
-	jumpif	sgt halt
-	jumpif	eq halt
+	shift	left r4 1
+	jumpif	nc halt
 	sub3	r2 r5 r2
 
 ; Halt program (the emulator will detect this and avoid looping forever)
---
+++ prog/mull.s
@@ -0,0 +1,63 @@
+;-----------------------------------------------------------------------------;
+;  Signed 64 * 64 -> 128 multiplication                                       ;
+;-----------------------------------------------------------------------------;
+
+; Initialization (program input)
+	leti	r0 -0x374bc563deb482
+	leti	r1 0x97b6af21f376
+	; r0 * r1 = 0xffffffdf'3ad8dfa1'591295b1'6f3f6614
+
+; Main program ([r3r2] = r0 * [r6r1])
+	leti	r2 0
+	leti	r3 0
+	leti	r6 0
+
+; Manage signs (r4 < 0 iff we need to swap signs after dividing)
+	xor3	r4 r0 r1
+	; We need to have 0 in a register to calculate rx = 0 - rx
+	leti	r5 0
+
+	cmpi	r0 0
+	jumpif	sgt r0_positive
+	sub3	r0 r5 r0
+r0_positive:
+	cmpi	r1 0
+	jumpif	sgt r1_positive
+	sub3	r1 r5 r1
+r1_positive:
+
+; Calculate the product!
+loop:
+	shift	right r0 1
+	jumpif	nc next
+
+	; Add with carry [r6r1] to [r3r2]
+	add2	r3 r6
+	add2	r2 r1
+	jumpif	nc next
+	add2i	r3 1
+
+next:
+	; Shift [r6r1] left 1 place
+	shift	left r6 1
+	shift 	left r1 1
+	jumpif	nc nc1
+	or2i	r6 1
+nc1:
+	cmpi	r0 0
+	jumpif	nz loop
+
+; Now swap signs if it was required
+	shift	left r4 1
+	jumpif	nc halt
+
+	; Perform susbtraction with carry
+	sub3	r2 r5 r2
+	jumpif	nc nc2
+	add2i	r3 1
+nc2:
+	sub3	r3 r5 r3
+
+; Halt program (the emulator will detect this and avoid looping forever)
+halt:
+	jump	-13
